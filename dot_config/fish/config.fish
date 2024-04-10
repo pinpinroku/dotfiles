edbc7b1b7f48b1bde9d25bac6f5c80030b5a00f4
pinpinroku <pinpinroku11600@tutanota.com>
2024-04-10 09:04:14 +0900

    bump:🔼 format fish config with fish_indent
    New formatter introduced in helix v24.03

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,14 +1,14 @@
 ## Set values
 # Hide welcome message & ensure we are reporting fish as shell
 set fish_greeting
-set VIRTUAL_ENV_DISABLE_PROMPT "1"
+set VIRTUAL_ENV_DISABLE_PROMPT 1
 set -xU MANPAGER "sh -c 'col -bx | bat -l man -p'"
-set -xU MANROFFOPT "-c"
+set -xU MANROFFOPT -c
 set -x SHELL /usr/bin/fish
 
 ## Export variable need for qt-theme
-if type "qtile" >> /dev/null 2>&1
-   set -x QT_QPA_PLATFORMTHEME "qt5ct"
+if type qtile >>/dev/null 2>&1
+    set -x QT_QPA_PLATFORMTHEME qt5ct
 end
 
 # Set settings for https://github.com/franciscolourenco/done
@@ -50,30 +50,31 @@ source /usr/share/doc/find-the-command/ftc.fish
 ## Functions
 # Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
 function __history_previous_command
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
+    switch (commandline -t)
+        case "!"
+            commandline -t $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
 end
 
 function __history_previous_command_arguments
-  switch (commandline -t)
-  case "!"
-    commandline -t ""
-    commandline -f history-token-search-backward
-  case "*"
-    commandline -i '$'
-  end
+    switch (commandline -t)
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
 end
 
-if [ "$fish_key_bindings" = fish_vi_key_bindings ];
-  bind -Minsert ! __history_previous_command
-  bind -Minsert '$' __history_previous_command_arguments
+if [ "$fish_key_bindings" = fish_vi_key_bindings ]
+    bind -Minsert ! __history_previous_command
+    bind -Minsert '$' __history_previous_command_arguments
 else
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
+    bind ! __history_previous_command
+    bind '$' __history_previous_command_arguments
 end
 
 # Fish command history
@@ -89,8 +90,8 @@ end
 function copy
     set count (count $argv | tr -d \n)
     if test "$count" = 2; and test -d "$argv[1]"
-	set from (echo $argv[1] | string trim --right --chars=/)
-	set to (echo $argv[2])
+        set from (echo $argv[1] | string trim --right --chars=/)
+        set to (echo $argv[2])
         command cp -r $from $to
     else
         command cp $argv
@@ -109,15 +110,15 @@ end
 # Replace ls with eza
 alias l 'eza --color=always --group-directories-first --icons'
 alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
-alias la 'eza -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll 'eza -l --color=always --group-directories-first --icons'  # long format
+alias la 'eza -a --color=always --group-directories-first --icons' # all files and dirs
+alias ll 'eza -l --color=always --group-directories-first --icons' # long format
 alias lt 'eza -aT --color=always --group-directories-first --icons' # tree listing
 alias l. 'eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
 
 # Replace some more things with better alternatives
 alias cat 'bat --style header --style snip --style changes --style header'
 if not test -x /usr/bin/yay; and test -x /usr/bin/paru
-    alias yay 'paru'
+    alias yay paru
 end
 
 
@@ -127,7 +128,7 @@ alias ... 'cd ../..'
 alias .... 'cd ../../..'
 alias ..... 'cd ../../../..'
 alias ...... 'cd ../../../../..'
-alias big 'expac -H M "%m\t%n" | sort -h | nl'     # Sort installed packages according to size in MB (expac must be installed)
+alias big 'expac -H M "%m\t%n" | sort -h | nl' # Sort installed packages according to size in MB (expac must be installed)
 alias dir 'dir --color=auto'
 alias fixpacman 'sudo rm /var/lib/pacman/db.lck'
 alias gitpkg 'pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
@@ -135,14 +136,14 @@ alias grep 'ugrep --color=auto'
 alias egrep 'ugrep -E --color=auto'
 alias fgrep 'ugrep -F --color=auto'
 alias grubup 'sudo update-grub'
-alias hw 'hwinfo --short'                          # Hardware Info
+alias hw 'hwinfo --short' # Hardware Info
 alias ip 'ip -color'
 alias psmem 'ps auxf | sort -nr -k 4'
 alias psmem10 'ps auxf | sort -nr -k 4 | head -10'
 alias rmpkg 'sudo pacman -Rdd'
 alias tarnow 'tar -acf '
 alias untar 'tar -zxvf '
-alias upd '/usr/bin/garuda-update'
+alias upd /usr/bin/garuda-update
 alias vdir 'vdir --color=auto'
 alias wget 'wget -c '
 
@@ -155,7 +156,7 @@ alias mirrors 'sudo reflector --latest 50 --number 20 --sort score --save /etc/p
 # Help people new to Arch
 alias apt 'man pacman'
 alias apt-get 'man pacman'
-alias please 'sudo'
+alias please sudo
 alias tb 'nc termbin.com 9999'
 alias helpme 'echo "To print basic information about a command use tldr <command>"'
 alias pacdiff 'sudo -H DIFFPROG=meld pacdiff'
@@ -168,7 +169,7 @@ alias rip 'expac --timefmt="%Y-%m-%d %T" "%l\t%n %v" | sort | tail -200 | nl'
 
 ## Run fastfetch if session is interactive
 if status --is-interactive && type -q fastfetch
-    if test $TERM = 'alacritty';
+    if test $TERM = alacritty
         neofetch
     else
         fastfetch --load-config dr460nized
@@ -183,9 +184,9 @@ alias v nvim
 alias nv neovide
 alias cp 'cp -iv'
 alias mv 'mv -iv'
-alias cl 'clear'
-alias cls 'clear'
-alias lsd 'eza'
+alias cl clear
+alias cls clear
+alias lsd eza
 alias note 'helix ~/Sync/note'
 alias list 'helix ~/Documents/input_list.txt'
 alias fig 'helix ~/.config/fish/config.fish'
@@ -218,9 +219,9 @@ alias gsw 'git switch'
 alias gl 'git log --graph --oneline --decorate --all'
 
 ## Environment variables ##
-set -x EDITOR '/usr/bin/helix'
-set -x VISUAL '/usr/bin/helix'
-set -x BAT_THEME 'Coldark-Dark'
+set -x EDITOR /usr/bin/helix
+set -x VISUAL /usr/bin/helix
+set -x BAT_THEME Coldark-Dark
 set -x FZF_DEFAULT_COMMAND 'fd --type file --color=always'
 set -x FZF_DEFAULT_OPTS '--ansi --reverse'
 
@@ -232,29 +233,29 @@ if test -d ~/.cargo/bin
 end
 
 # Shell completion for poetry
-poetry completions fish > ~/.config/fish/completions/poetry.fish
+poetry completions fish >~/.config/fish/completions/poetry.fish
 
 # `ya` shell wrapper that provides the ability
 # to change the current working directory when exiting Yazi
 function ya
-	set tmp (mktemp -t "yazi-cwd.XXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
 end
 
 ## Music utils ##
 function play-random-album
     set -l playlist '/tmp/random_album.m3u8'
-    fd . -td --min-depth 3 -a ~/Music | shuf > $playlist
+    fd . -td --min-depth 3 -a ~/Music | shuf >$playlist
     mpv --profile=music $playlist
 end
 
 ## Wine settings and aliases ##
 set -x LANG 'ja_JP.utf8'
-set -x WINEARCH 'win32'
+set -x WINEARCH win32
 
 function fb2k
     set -xl WINEPREFIX "$HOME/WindowsApps/foobar2000"
