ca00f694400555c3662f4f8f4e41264670525ec9
pinpinroku <pinpinroku11600@tutanota.com>
2025-08-05 12:38:59 +0900

    ✨ Cleanup fish configs

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -1,96 +1,113 @@
-## Aliases ##
-
 ## Common ##
-alias e='/usr/bin/yazi'
-alias hx='/usr/bin/helix'
-alias cz='/usr/bin/chezmoi -v'
-alias cl=clear
-alias cp='cp -iv'
-alias mv='mv -iv'
-alias rm='rm -v'
-alias ip='ip -color=always'
-alias mkdir='mkdir -v'
-alias cat='bat --style header --style snip --style changes --style header'
+abbr -a e /usr/bin/yazi
+abbr -a hx /usr/bin/helix
+abbr -a cz '/usr/bin/chezmoi -v'
+abbr -a c =clear
+abbr -a cp 'cp -iv'
+abbr -a mv 'mv -iv'
+abbr -a rm 'rm -v'
+abbr -a ip 'ip -color=always'
+abbr -a mkdir 'mkdir -v'
+abbr -a cat 'bat --style header --style snip --style changes --style header'
+abbr -a fd 'fd -c always'
 
 ## Edit Documents ##
-alias todo='helix --working-dir ~/note/ ~/note/todo.md'
-alias note='helix --working-dir ~/note/'
-alias fig='helix --working-dir ~/.config/fish/ ~/.config/fish/conf.d/profile.fish'
+abbr -a todo 'helix --working-dir ~/note/ ~/note/todo.md'
+abbr -a note 'helix --working-dir ~/note/'
+abbr -a fig 'helix --working-dir ~/.config/fish/ ~/.config/fish/conf.d/profile.fish'
 
 ## File Operation ##
-alias chmod='chmod -c'
-alias fzp='fzf --preview="bat --color=always --style=numbers --line-range=:500 {}" --preview-window="right:50%,border-vertical"'
-abbr -a erase 'fd -tf -e jpg -e png -e jpeg -x exiftool -overwrite_original -all= {}' # Remove all image metadata
-abbr -a mine 'fd -tf -e mp4 -e mkv -x chmod -c 0640'
+abbr -a chmod 'chmod -c'
+abbr -a fzp 'fzf --preview="bat --color=always --style=numbers --line-range=:500 {}" --preview-window="right:50%,border-vertical"'
+abbr -a erase 'fd -tf -g "*.{png,jpg,jpeg}" -x exiftool -overwrite_original -all= {}' # Remove all image metadata
+abbr -a mine 'fd -tf -g "*.{mp4,mkv}" -x chmod -c 0640'
+abbr -a mkt 'mktemp -p /tmp '
 
 ## System Analyze ##
-alias du='du -sh'
-alias df='df -h'
-alias top='btm --basic'
-alias htop='btm --basic'
-alias memory='free -h'
-alias hwinfo='hwinfo --short'
-alias glxinfo='glxinfo -B'
-alias vulkaninfo='vulkaninfo --summary'
+abbr -a df 'df -h'
+abbr -a du 'du -sh'
+abbr -a top 'btm --basic'
+abbr -a htop 'btm --basic'
+abbr -a dust 'dust -b --skip-total'
+abbr -a memory 'free -h'
+abbr -a hwinfo 'hwinfo --short'
+abbr -a glxinfo 'glxinfo -B'
+abbr -a vulkaninfo 'vulkaninfo --summary'
+abbr -a duf \
+    'duf \
+    -hide-fs vfat,devtmpfs,efivarfs \
+    -hide-mp "/,/root,/srv,/dev/shm,/var/*,/run/credentials/*" \
+    -theme ansi \
+    -style ascii'
 
 ## Replace ls with eza ##
 alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
 alias la='eza -a --color=always --group-directories-first --icons' # all files and dirs
 alias ll='eza -l --color=always --group-directories-first --icons' # long format
-alias lt='eza -aT --color=always --group-directories-first --icons --git-ignore' # tree listing
-alias l.="eza -a | grep -e '^\.'" # show only dotfiles
+alias lt='eza -T --color=always --group-directories-first --icons --git-ignore' # tree listing
 alias l='eza --color=always --group-directories-first --icons --sort Name' # column listing
 
 ## Cleanup ##
-alias forget='qdbus6 org.kde.klipper /klipper org.kde.klipper.klipper.clearClipboardHistory'
+abbr -a forget 'qdbus6 org.kde.klipper /klipper org.kde.klipper.klipper.clearClipboardHistory'
 abbr -a remove-empty-journal 'fd . -tf --size -23b ~/journal -x rm -v' # Remove journals with header only
 
 ## Media Editing ##
 alias ffmpeg='ffmpeg -hide_banner'
 alias ffprobe='ffprobe -hide_banner'
 
+## Media player ##
+abbr -a mna 'mpv --no-resume-playback --no-audio'
+abbr -a mpm 'mpv --profile=music'
+
 ## Zellij: Terminal Multiplexer ##
 alias zl='/usr/bin/zellij'
-alias zla='zellij attach'
-alias zls='zellij list-sessions'
-alias zld='zellij delete-session'
-alias zlk='zellij kill-session'
+abbr -a zla 'zellij attach'
+abbr -a zls 'zellij list-sessions'
+abbr -a zld 'zellij delete-session'
+abbr -a zlk 'zellij kill-session'
 
 ## systemctl ##
-alias timers='systemctl list-timers -a'
-alias units='systemctl list-unit-files --type=service'
+abbr -a timers 'systemctl list-timers -a'
+abbr -a units 'systemctl list-unit-files --type=service'
 abbr -a sdr 'sudo systemctl daemon-reload'
 
 ## pacman ##
-alias uninstall="pacman -Qeq | fzf --multi --preview 'pacman -Qi {}' | xargs -ro sudo pacman -Rns"
-alias foreign='pacman -Qm'
+abbr -a uninstall "pacman -Qeq | fzf --multi --preview 'pacman -Qi {}' | xargs -ro sudo pacman -Rns"
+abbr -a foreign 'pacman -Qm'
 abbr -a ditch 'sudo pacman -Rns'
-
-## Network Debugging ##
-alias wifi='journalctl -ru iwd.service'
-alias adhm='journalctl -ru AdGuardHome.service'
+abbr -a search "pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S"
+abbr -a lookup "pacman -Qeq | fzf --multi --preview 'pacman -Qi {}'"
 
 ## Journal ##
-alias jctl="journalctl -p 3 -xb"
-alias jf='journalctl -f'
-alias jeb='journalctl -eb'
-abbr -a jcg 'journalctl -b -g'
+abbr -a jctl 'journalctl -p 3 -xb'
+abbr -a jf 'journalctl -f'
+abbr -a jeb 'journalctl -eb'
+abbr -a jcg --ser-cursor 'journalctl -b -g \'%\''
+
+## Network Debugging ##
+abbr -a wifi 'journalctl -b -eu iwd.service'
+abbr -a adhm 'journalctl -b -eu AdGuardHome.service'
+abbr -a ports 'ss -tulne4'
 
 ## git ##
-alias gs='git status --short --branch'
-alias ga='git add'
-alias gp='git push'
-alias gr='git remote -v'
-alias gb='git branch -av'
-alias gbd='git branch -D'
-alias gc='git commit -v'
-alias gd='git diff'
-alias gm='git merge'
-alias gsw='git switch'
-alias gl='git log --graph --oneline --decorate --all'
+abbr -a gs 'git status --short --branch'
+abbr -a ga 'git add'
+abbr -a gp 'git push'
+abbr -a gr 'git remote -v'
+abbr -a gb 'git branch -av'
+abbr -a gbd 'git branch -D'
+abbr -a gc 'git commit -v'
+abbr -a gd 'git diff'
+abbr -a gm 'git merge'
+abbr -a gsw 'git switch'
+abbr -a gl 'git log --graph --oneline --decorate --all'
 abbr -a grp 'git reset HEAD~' # Cancel previous commit and revert the commit back to staging area
 abbr -a grb 'git rebase -i HEAD~' # Squash multiple commits into one
 abbr -a gfp 'git fetch; and git pull' # Fetch and pull (`--prune` is always active in `~/.gitconfig`)
 
+## Virtual Machine ##
+abbr -a qimg --set-cursor 'qemu-img create -f qcow2 %.qcow2 -o nocow=on 50G'
+
 ## Celeste ##
-abbr -a modup 'hultra -m "wegfan,jade,otobot,gb" update --install'
+abbr -a modupd 'hultra -m "wegfan,jade,otobot,gb" update --install'
+abbr -a modins 'hultra -m "wegfan,jade,otobot,gb" install '
--- dot_config/fish/conf.d/profile.fish
+++ dot_config/fish/conf.d/profile.fish
@@ -1,145 +1,34 @@
-### Initialization ###
-
-## Initial Setup ##
-
-# Starship Prompt
-set -gx STARSHIP_CONFIG "$HOME/.config/starship/starship-mokka.toml"
-
 if status --is-interactive
+    # Starshipt prompt
+    set -gx STARSHIP_CONFIG ~/.config/starship/starship-mokka.toml
+
+    # Text editor
+    set -gx EDITOR /usr/bin/helix
+    set -gx VISUAL /usr/bin/helix
+
+    # fzf: A command-line fuzzy finder
+    set -gx FZF_DEFAULT_OPTS '--ansi --reverse'
+    set -gx FZF_DEFAULT_COMMAND 'fd \
+        --type file \
+        --strip-cwd-prefix \
+        --hidden \
+        --follow \
+        --exclude .git \
+        --color=always'
+
+    ## Autostart zellij ##
+    set ZELLIJ_AUTO_ATTACH true
+    set ZELLIJ_AUTO_EXIT true
+    eval (zellij setup --generate-auto-start fish | string collect)
+
     ## Set vi mode ##
     fish_vi_key_bindings
     set fish_cursor_default block
     set fish_cursor_insert line
-    ## init starship ##
+
+    ## Initialize starship ##
     starship init fish | source
-    ## init zoxide ##
+
+    ## Initialize zoxide ##
     zoxide init fish | source
-end
-
-# Disable welcome prompt
-function fish_mode_prompt
-end
-
-### Function ###
-
-## Downloader ##
-
-# Download videos from specific source
-function download_videos_from
-    # Check if an argument is provided
-    if test (count $argv) -eq 0
-        echo "Error: No configuration name provided. Please specify one of: tver, audio, tube."
-        return 1
-    end
-
-    # Define valid config file names
-    set -l valid_configs tver audio tube
-
-    # Check if the provided argument is valid
-    if not contains $argv $valid_configs
-        echo "Error: Invalid configuration name '$argv'. Valid options are: $valid_configs."
-        return 1
-    end
-
-    # Creates temporary directory and the file to write URLs
-    set -l tmpdir (mktemp -dp '/tmp' 'yt-dlp.XXXXXXXXXX')
-    set -l input_file (mktemp -p "$tmpdir" --suffix '.txt' 'url-list.XXXXXXXXXX')
-
-    # Open the file to edit URLs
-    $EDITOR $input_file
-
-    # Download videos with yt-dlp using specific config file
-    yt-dlp_linux \
-        --ignore-config \
-        --config-location "$HOME/.config/yt-dlp/$argv.conf" \
-        --path "temp: $tmpdir" \
-        --batch-file "$input_file"
-
-    # Delete temporary directory if previous yt-dlp command succeed
-    and rm "$tmpdir" -rf
-end
-
-# TODO: Might not need this already
-# Download video with specific name using aria2c
-function dlvid
-    yt-dlp --verbose --output "$argv[1].%(ext)s" $argv[2]
-end
-
-# Run the downloader for TVer
-function tvdl
-    if test -d "$HOME/.config/tver-dl"; and type -q tver-dl
-        set -l result (tver-dl)
-        if not test -z "$result"
-            printf "%s\n" $result | tee /dev/tty | yt-dlp_linux \
-                --ignore-config \
-                --config-location "$HOME/.config/tver-dl/yt-dlp.conf"
-        else
-            echo "No recent uploads."
-        end
-    end
-end
-
-## File Manager ##
-# shell wrapper that provides the ability to change the current working directory when exiting Yazi
-function y
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    yazi $argv --cwd-file="$tmp"
-    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        builtin cd -- "$cwd"
-    end
-    rm -f -- "$tmp"
-end
-
-## Music Utils ##
-# Play single album
-function select_and_play_album
-    set --local music_dir "$HOME/Music"
-    set --local min_depth 3
-
-    set --local album_dir (fd . \
-        --type directory \
-        --color always \
-        --min-depth $min_depth \
-        $music_dir | fzf --preview 'eza --color=always --tree --icons {}')
-
-    if test -n "$album_dir"
-        mpv --profile=music "$album_dir"
-    else
-        echo "No album selected."
-    end
-end
-
-# Play random album
-function play-random-album
-    set --local playlist (mktemp -p '/tmp' --suffix '.m3u8' 'random-playlist.XXXXXXXXXX')
-    fd . --type directory --min-depth 3 --absolute-path "$HOME/Music" | shuf >$playlist
-    mpv --profile=music $playlist; and rm $playlist
-end
-
-## Note Taking ##
-# Create or Open Journal
-function memo
-    set --local notePath "$HOME/journal"
-    set --local noteFilename "$notePath/note-$(date +%Y-%m-%d).md"
-
-    if not test -f $noteFilename
-        echo "# Notes for $(date +%Y-%m-%d)" >$noteFilename
-    end
-
-    helix --working-dir $notePath $noteFilename
-end
-
-## Ignore History ##
-function fish_should_add_to_history
-    for cmd in exit wiki z ls fastfetch
-        string match --quiet --regex "^$cmd" -- $argv; and return 1
-    end
-    # Check for Japanese characters in arguments
-    string match --quiet --regex '[\\p{Han}\\p{Hiragana}\\p{Katakana}]' -- $argv; and return 1
-    return 0
-end
-
-## Decrease clipped audio volume ##
-function lower_volume
-    ffmpeg -i "$argv" -af 'volume=0.1' -c:v copy "_($argv)"
 end
